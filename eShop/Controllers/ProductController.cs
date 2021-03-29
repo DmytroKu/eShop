645b89c10eb08323ff86bf183dd1d0acd56e15f3
DmytroKu <deamand789@gmail.com>
2021-03-29 21:32:23 +0300

    Add Product controller,view

---
+++ eShop/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using eShop.Models;
+using eShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.Controllers
+{
+    public class ProductController: Controller
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            var productsListViewModel = new ProductsListViewModel
+            {
+                Products = _productRepository.AllProduct,
+                Categories = _categoryRepository.AllCategories
+            };
+            return View(productsListViewModel);
+        }
+    }
+}
---
+++ eShop/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using eShop.Models;
+
+namespace eShop.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
